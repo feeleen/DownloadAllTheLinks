915e6e3400f1c25af77b7b87ac734af914ed5073
bigware <bee-four@rambler.ru>
2016-10-19 20:55:12 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace DownloaderAllTheLinks
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.OfficeBox = new System.Windows.Forms.CheckBox();
+			this.ArchivesBox = new System.Windows.Forms.CheckBox();
+			this.ImagesBox = new System.Windows.Forms.CheckBox();
+			this.CheckAllBtn = new System.Windows.Forms.Button();
+			this.UncheckAllBtn = new System.Windows.Forms.Button();
+			this.FolderBox = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.UseFileNamesBox = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.listView1.CheckBoxes = true;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+			this.listView1.GridLines = true;
+			this.listView1.Location = new System.Drawing.Point(12, 86);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(710, 318);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "URL";
+			this.columnHeader1.Width = 414;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Name";
+			this.columnHeader2.Width = 286;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(728, 9);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 56);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "1. Parse Url";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(710, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 70);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(98, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "List Of Found Items";
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button2.Location = new System.Drawing.Point(728, 86);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 56);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "2. Download";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// OfficeBox
+			// 
+			this.OfficeBox.AutoSize = true;
+			this.OfficeBox.Checked = true;
+			this.OfficeBox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.OfficeBox.Location = new System.Drawing.Point(15, 41);
+			this.OfficeBox.Name = "OfficeBox";
+			this.OfficeBox.Size = new System.Drawing.Size(78, 17);
+			this.OfficeBox.TabIndex = 4;
+			this.OfficeBox.Text = "Office Files";
+			this.OfficeBox.UseVisualStyleBackColor = true;
+			// 
+			// ArchivesBox
+			// 
+			this.ArchivesBox.AutoSize = true;
+			this.ArchivesBox.Checked = true;
+			this.ArchivesBox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.ArchivesBox.Location = new System.Drawing.Point(101, 41);
+			this.ArchivesBox.Name = "ArchivesBox";
+			this.ArchivesBox.Size = new System.Drawing.Size(67, 17);
+			this.ArchivesBox.TabIndex = 4;
+			this.ArchivesBox.Text = "Archives";
+			this.ArchivesBox.UseVisualStyleBackColor = true;
+			// 
+			// ImagesBox
+			// 
+			this.ImagesBox.AutoSize = true;
+			this.ImagesBox.Checked = true;
+			this.ImagesBox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.ImagesBox.Location = new System.Drawing.Point(187, 41);
+			this.ImagesBox.Name = "ImagesBox";
+			this.ImagesBox.Size = new System.Drawing.Size(60, 17);
+			this.ImagesBox.TabIndex = 4;
+			this.ImagesBox.Text = "Images";
+			this.ImagesBox.UseVisualStyleBackColor = true;
+			// 
+			// CheckAllBtn
+			// 
+			this.CheckAllBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.CheckAllBtn.Location = new System.Drawing.Point(728, 148);
+			this.CheckAllBtn.Name = "CheckAllBtn";
+			this.CheckAllBtn.Size = new System.Drawing.Size(75, 23);
+			this.CheckAllBtn.TabIndex = 1;
+			this.CheckAllBtn.Text = "Check All";
+			this.CheckAllBtn.UseVisualStyleBackColor = true;
+			this.CheckAllBtn.Click += new System.EventHandler(this.CheckAllBtn_Click);
+			// 
+			// UncheckAllBtn
+			// 
+			this.UncheckAllBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.UncheckAllBtn.Location = new System.Drawing.Point(728, 177);
+			this.UncheckAllBtn.Name = "UncheckAllBtn";
+			this.UncheckAllBtn.Size = new System.Drawing.Size(75, 23);
+			this.UncheckAllBtn.TabIndex = 1;
+			this.UncheckAllBtn.Text = "Uncheck All";
+			this.UncheckAllBtn.UseVisualStyleBackColor = true;
+			this.UncheckAllBtn.Click += new System.EventHandler(this.UncheckAllBtn_Click);
+			// 
+			// FolderBox
+			// 
+			this.FolderBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.FolderBox.Location = new System.Drawing.Point(87, 410);
+			this.FolderBox.Name = "FolderBox";
+			this.FolderBox.Size = new System.Drawing.Size(529, 20);
+			this.FolderBox.TabIndex = 2;
+			// 
+			// button3
+			// 
+			this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button3.Location = new System.Drawing.Point(622, 408);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(100, 23);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "Browse Folder ...";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 413);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(72, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Save Files To";
+			// 
+			// UseFileNamesBox
+			// 
+			this.UseFileNamesBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.UseFileNamesBox.AutoSize = true;
+			this.UseFileNamesBox.Checked = true;
+			this.UseFileNamesBox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.UseFileNamesBox.Location = new System.Drawing.Point(622, 66);
+			this.UseFileNamesBox.Name = "UseFileNamesBox";
+			this.UseFileNamesBox.Size = new System.Drawing.Size(100, 17);
+			this.UseFileNamesBox.TabIndex = 4;
+			this.UseFileNamesBox.Text = "Use File Names";
+			this.UseFileNamesBox.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(815, 440);
+			this.Controls.Add(this.UseFileNamesBox);
+			this.Controls.Add(this.ImagesBox);
+			this.Controls.Add(this.ArchivesBox);
+			this.Controls.Add(this.OfficeBox);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.FolderBox);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.UncheckAllBtn);
+			this.Controls.Add(this.CheckAllBtn);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listView1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Download All The Links";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.CheckBox OfficeBox;
+		private System.Windows.Forms.CheckBox ArchivesBox;
+		private System.Windows.Forms.CheckBox ImagesBox;
+		private System.Windows.Forms.Button CheckAllBtn;
+		private System.Windows.Forms.Button UncheckAllBtn;
+		private System.Windows.Forms.TextBox FolderBox;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.CheckBox UseFileNamesBox;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿//using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloaderAllTheLinks
+{
+	public partial class Form1 : Form
+	{
+		private int sortColumn = -1;
+		//RootObject root = null;
+
+		List<string> office = new List<string> { "pdf", "doc", "docx", "xls", "xlsx", "odt", "rtf", "txt", "ppt", "pptx", "ods","xps","csv"};
+		List<string> images = new List<string> { "png", "tiff", "jpg", "jpeg", "gif", "ico"};
+		List<string> archives = new List<string> { "rar", "zip", "tar", "7z"};
+
+		string placeholder = "placeholder";
+		string regPattern = @"(<a.+href=""([^\""]+\.(placeholder)){1}\""([^>]*)>((?:.(?!\<\/a\>))*.)<\/a>)";
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			string downloadsPath = AppDomain.CurrentDomain.BaseDirectory + "Downloads";
+			if (!Directory.Exists(downloadsPath))
+			{
+				Directory.CreateDirectory(downloadsPath);
+			}
+			FolderBox.Text = downloadsPath;
+			
+		}
+
+		private void LoadWebPage(string url)
+		{
+			using (WebClient client = new WebClient())
+			{
+				client.Encoding = Encoding.UTF8;
+				client.DownloadStringCompleted += Client_DownloadStringCompleted;
+				client.DownloadStringAsync(new Uri(url));
+			}
+		}
+
+		private void Client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+		{
+			PopulateList(e.Result);
+		}
+
+		private void PopulateList(string text)
+		{
+			listView1.Items.Clear();
+
+			List<string> fileTypes = new List<string>();
+			if (OfficeBox.Checked)
+				fileTypes = fileTypes.Concat(office).ToList();
+			if (ArchivesBox.Checked)
+				fileTypes = fileTypes.Concat(archives).ToList();
+			if (ImagesBox.Checked)
+				fileTypes = fileTypes.Concat(images).ToList();
+
+			string CurrentRegPattern = regPattern.Replace(placeholder, String.Join("|", fileTypes));
+			Regex reg = new Regex(CurrentRegPattern, RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+
+			Uri host = new Uri(textBox1.Text);
+			foreach (Match m in reg.Matches(text))
+			{
+				string link = m.Groups[2].Value;
+				string fullLink = "";
+				if (link.StartsWith("/"))
+					fullLink = host.Scheme + Uri.SchemeDelimiter + host.Host + link;
+				else if (link.StartsWith("http"))
+					fullLink = link;
+				else
+					fullLink = host.Scheme + Uri.SchemeDelimiter + host.Host + "/" + link;
+
+				ListViewItem lvi = new ListViewItem(fullLink);
+				if (UseFileNamesBox.Checked)
+					lvi.SubItems.Add(new Uri(fullLink).Segments.LastOrDefault());
+				else
+					lvi.SubItems.Add(m.Groups[5].Value);
+				lvi.Checked = true;
+				listView1.Items.Add(lvi); 
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			LoadWebPage(textBox1.Text);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string dir = FolderBox.Text;
+			foreach (ListViewItem lvi in listView1.Items)
+			{
+				if (lvi.Checked)
+				{
+					string localFile = dir + "\\" + lvi.SubItems[1].Text + "." + lvi.Text.Substring(lvi.Text.LastIndexOf('.'));
+					localFile = localFile.Replace("...", ".").Replace("..", ".");
+					string UriString = lvi.Text;
+					using (WebClient client = new WebClient())
+					{
+						client.DownloadFileAsync(new Uri(UriString), localFile);
+						//client.DownloadFile(new Uri(UriString), localFile);
+					}
+				}
+				//break;
+			}
+		}
+
+		/*
+		public static T GetObject<T>(string text)
+		{
+			T obj = JsonConvert.DeserializeObject<T>(text);
+			return obj;
+		}
+		*/
+
+		private void CheckAllBtn_Click(object sender, EventArgs e)
+		{
+			SetCheckState(true);
+		}
+
+		private void SetCheckState(bool v)
+		{
+			foreach (ListViewItem lvi in listView1.Items)
+			{
+				lvi.Checked = v;
+			}
+		}
+
+		private void UncheckAllBtn_Click(object sender, EventArgs e)
+		{
+			SetCheckState(false);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+			if (fbd.ShowDialog() == DialogResult.OK)
+			{
+				if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+				{
+					FolderBox.Text = fbd.SelectedPath;
+				}
+			}
+		}
+
+		private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+		{
+			// Determine whether the column is the same as the last column clicked.
+			if (e.Column != sortColumn)
+			{
+				// Set the sort column to the new column.
+				sortColumn = e.Column;
+				// Set the sort order to ascending by default.
+				listView1.Sorting = SortOrder.Ascending;
+			}
+			else
+			{
+				// Determine what the last sort order was and change it.
+				if (listView1.Sorting == SortOrder.Ascending)
+					listView1.Sorting = SortOrder.Descending;
+				else
+					listView1.Sorting = SortOrder.Ascending;
+			}
+
+			// Call the sort method to manually sort.
+			listView1.Sort();
+			// Set the ListViewItemSorter property to a new ListViewItemComparer
+			// object.
+			this.listView1.ListViewItemSorter = new ListViewItemComparer(e.Column, listView1.Sorting);
+		}
+	}
+
+	public class Link
+	{
+		public string Url { get; set; }
+		public string Name { get; set; }
+	}
+
+	public class RootObject
+	{
+		public List<Link> Links { get; set; }
+	}
+
+	public class ListViewItemComparer : IComparer
+	{
+
+		private int col;
+		private SortOrder order;
+		public ListViewItemComparer()
+		{
+			col = 0;
+			order = SortOrder.Ascending;
+		}
+		public ListViewItemComparer(int column, SortOrder order)
+		{
+			col = column;
+			this.order = order;
+		}
+		public int Compare(object x, object y)
+		{
+			int returnVal = -1;
+			returnVal = String.Compare(((ListViewItem)x).SubItems[col].Text,
+							((ListViewItem)y).SubItems[col].Text);
+			// Determine whether the sort order is descending.
+			if (order == SortOrder.Descending)
+				// Invert the value returned by String.Compare.
+				returnVal *= -1;
+			return returnVal;
+		}
+	}
+}
